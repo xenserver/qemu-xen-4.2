0adde32edabc7de93530b627d3c982ffb912bdb9
Ian Jackson <iwj@mariner.uk.xensource.com>
2008-05-09 17:07:37 +0100

    Clear TD status field explicitly when it's fetched.
    changeset:   13574:b064775fba7d838c99bcf11ca4fec6127e0e8792
    user:        kfraser@localhost.localdomain
    date:        Wed Jan 24 10:44:32 2007 +0000
    files:       tools/ioemu/hw/usb-uhci.c
    description:
    [QEMU] Clear TD status field explicitly when it's fetched.
    
    In current Qemu-dm, UHC will set some status bits of TD in status
    updating stage, but never process the status bit if relevant condition
    does not occur, leaving it as it is. When a TD is fetched with some
    status bits are set to 1, it will return to Guest OS with these bits
    set to 1 even this TD is executed successfully. Some Windows OS,
    e.g. Windows 2000, will check status bits of TD in UHC interrupt
    routine, treat it as a unsuccessful one if some status bits are set to
    1 and discard the data. Other Windows OS just check USBSTS of UHC,
    ignoring status field of TD unless the value of USBSTS indicates
    occurrence of error.
    
    With this patch, USB mouse/tablet in Windows 2000 works correctly.
    
    Signed-off-by: Xinmei Huang  <xinmei.huang@intel.com>

--- hw/usb-uhci.c
+++ hw/usb-uhci.c
@@ -49,9 +49,15 @@
 #define TD_CTRL_IOC     (1 << 24)
 #define TD_CTRL_ACTIVE  (1 << 23)
 #define TD_CTRL_STALL   (1 << 22)
+#define TD_CTRL_BUFFER  (1 << 21)
 #define TD_CTRL_BABBLE  (1 << 20)
 #define TD_CTRL_NAK     (1 << 19)
 #define TD_CTRL_TIMEOUT (1 << 18)
+#define TD_CTRL_BITSTUFF                                 \
+                        (1 << 17)
+#define TD_CTRL_MASK                                     \
+    (TD_CTRL_BITSTUFF | TD_CTRL_TIMEOUT | TD_CTRL_NAK    \
+     | TD_CTRL_BABBLE | TD_CTRL_BUFFER | TD_CTRL_STALL)
 
 #define UHCI_PORT_RESET (1 << 9)
 #define UHCI_PORT_LSDA  (1 << 8)
@@ -518,6 +524,8 @@ static int uhci_handle_td(UHCIState *s, UHCI_TD *td, uint32_t *int_mask,
     if (td->ctrl & TD_CTRL_IOC) {
         *int_mask |= 0x01;
     }
+    /* Clear TD's status field explicitly */
+    td->ctrl = td->ctrl & (~TD_CTRL_MASK);
 
     if (!(td->ctrl & TD_CTRL_ACTIVE))
         return 1;
