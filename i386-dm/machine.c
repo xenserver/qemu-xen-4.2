3ace6c9b84cbde0959fd97e606a21e2d959f1f04
Ian Jackson <ian@davenant.greenend.org.uk>
2008-05-30 17:29:38 +0100

    Add some #includes

--- i386-dm/machine.c
+++ i386-dm/machine.c
@@ -1,7 +1,14 @@
+#include "hw/hw.h"
+#include "hw/boards.h"
+#include "hw/pc.h"
+#include "hw/isa.h"
+
+#include "exec-all.h"
+
 void cpu_save(QEMUFile *f, void *opaque) { }
 int cpu_load(QEMUFile *f, void *opaque, int version_id) { return 0; }
 
-static void register_machines(void)
+void register_machines(void)
 {
     qemu_register_machine(&xenfv_machine);
     qemu_register_machine(&xenpv_machine);
