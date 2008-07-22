ec990f69746593cfcd9c1bb06ba849576086bbe2
Ian Jackson <ian.jackson@eu.citrix.com>
2008-07-22 11:52:24 +0100

    pci passthrough changes ported across from xen-unstable by Yuji Shimada
    The patch contains following patches which have been applied to Xen
    Unstable Changeset 17959-18076.
    
    pci_config_passthrough_with_msi_msix.patch
    
        [Xen-devel] [PATCH] Support more Capability Structures (including
        MSI/MSI-X) and Device Specific Registers for pt device.
        Yuji Shimada <shimada-yxb@necst.nec.co.jp>
        Fri, 04 Jul 2008 15:26:52 +0900
    
    fix_pt_iomap.patch
    
        Re: [Xen-devel] [PATCH] Support more Capability Structures
        (including MSI/MSI-X) and Device Specific Registers for pt device.
        NISHIGUCHI Naoki <nisiguti@jp.fujitsu.com>
        Tue, 08 Jul 2008 14:14:55 +0900
    
    fix_pt_bar_mapping.patch
    
        [Xen-devel] [PATCH] ioemu: pass-through: pt_bar_mapping fix
        NISHIGUCHI Naoki <nisiguti@jp.fujitsu.com>
        Wed, 09 Jul 2008 15:46:15 +0900
    
    fix_hot-remove.patch
    
        [Xen-devel] [PATCH] Fix issues when passthrough device is
        hot-removed from HVM domain
        Yuji Shimada <shimada-yxb@necst.nec.co.jp>
        Wed, 16 Jul 2008 09:20:11 +0900
    
    chk_bar_overlap_bugfix.patch
    
        [Xen-devel] [PATCH] ioemu: alternative patch fixing pt_chk_bar_overlap
        Yuji Shimada <shimada-yxb@necst.nec.co.jp>
        Thu, 17 Jul 2008 16:05:11 +0900
    
    Thanks
    
    Signed-off-by: Yuji Shimada <shimada-yxb@necst.nec.co.jp>

--- hw/pci.h
+++ hw/pci.h
@@ -78,6 +78,8 @@ void pci_register_io_region(PCIDevice *pci_dev, int region_num,
                             uint32_t size, int type,
                             PCIMapIORegionFunc *map_func);
 
+int pt_chk_bar_overlap(PCIBus *bus, int devfn, uint32_t addr, uint32_t size);
+
 uint32_t pci_default_read_config(PCIDevice *d,
                                  uint32_t address, int len);
 void pci_default_write_config(PCIDevice *d,
