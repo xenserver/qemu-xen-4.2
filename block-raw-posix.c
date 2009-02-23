xenserver/qemu-xen-4.2

commit 8adde1cbba18de2704a30a76f20855f89c664a88

fix raw_aio_remove list walk bug
Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

Cherry picked and cross-ported by Ian Jackson
from Stefano's submission to qemu-devel, 18 Feb 2009 16:56:16 GMT.

block-raw-posix.c
@@ -673,6 +673,7 @@ static void raw_aio_remove(RawAIOCB *acb)
     pacb = &posix_aio_state->first_aio;
     for(;;) {
         if (*pacb == NULL) {
+            fprintf(stderr, "raw_aio_remove: aio request not found!\n");
             break;
         } else if (*pacb == acb) {
             *pacb = acb->next;
@@ -680,7 +681,7 @@ static void raw_aio_remove(RawAIOCB *acb)
             qemu_aio_release(acb);
             break;
         }
-        pacb = &acb->next;
+        pacb = &(*acb)->next;
     }
 }
 

commit 4969f3636ca57841d378376212c693caa1541cca

fix raw_aio_remove list walk bug (really)
Fix typo in my backport of Stefano's fix, which broke the build
in 8adde1cbba18de2704a30a76f20855f89c664a88.

block-raw-posix.c
@@ -681,7 +681,7 @@ static void raw_aio_remove(RawAIOCB *acb)
             qemu_aio_release(acb);
             break;
         }
-        pacb = &(*acb)->next;
+        pacb = &(*pacb)->next;
     }
 }
 