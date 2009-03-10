8e6c11d437513867d4ba62b1495cb24fd8985a16
Ian Jackson <ian.jackson@eu.citrix.com>
2009-03-10 18:13:07 +0000

    fix screendump (Stefano Stabellini)
    this patch fixes the screendump functionality that was recently broken;
    it must be applied *after* PATCH 5, 6 and 7 of the original displaystate
    change patch series.
    In fact the other patches make much easier to solve the screendump
    problem because they make the console switching mechanism more robust.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6345 c046a42c-6fe2-441c-8c8c-71466251a162
    
    As adapted by Stefano to qemu-xen-unstable in his posting:
     [PATCH 9 of 13] fix screendump
    which also contains several following fixes.

--- hw/vga_int.h
+++ hw/vga_int.h
@@ -191,6 +191,7 @@ void vga_common_init(VGAState *s, uint8_t *vga_ram_base,
 uint32_t vga_mem_readb(void *opaque, target_phys_addr_t addr);
 void vga_mem_writeb(void *opaque, target_phys_addr_t addr, uint32_t val);
 void vga_invalidate_scanlines(VGAState *s, int y1, int y2);
+int ppm_save(const char *filename, struct DisplaySurface *ds);
 
 void vga_draw_cursor_line_8(uint8_t *d1, const uint8_t *src1,
                             int poffset, int w,
