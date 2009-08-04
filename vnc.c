99a5a9155284ab47eda2be738be0115aa97ce0ff
Ian Jackson <ian.jackson@eu.citrix.com>
2009-08-04 15:35:46 +0100

    fix vnc screen corruption bugs and viewer exits
    I have been having problems with display corruption using raw
    encoding.
    
    I have also been having issues with the vncviewer client exiting with
    "rect too big".
    
    These problems appear when there are rapid changes to the framebuffer
    contents. [I've been using "find /" in my tests to generate a lot of text
    output.]
    
    The root cause of both problems appears to be the changing of
    the underlying framebuffer data during the creation of the update.
    [Hextile makes multiple passes over the data and gets badly
    confused if the data changes..]
    
    The attached patch pulls the pixel data from the "stable" old_data
    buffer rather than directly from the (changing) display surface.
    
    Submitted-by: Andrew Thomas <andrew.thomas@oracle.com>
    Signed-off-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- vnc.c
+++ vnc.c
@@ -499,7 +499,7 @@ static void send_framebuffer_update_raw(VncState *vs, int x, int y, int w, int h
 
     vnc_framebuffer_update(vs, x, y, w, h, 0);
 
-    row = ds_get_data(vs->ds) + y * ds_get_linesize(vs->ds) + x * ds_get_bytes_per_pixel(vs->ds);
+    row = vs->old_data + y * ds_get_linesize(vs->ds) + x * ds_get_bytes_per_pixel(vs->ds);
     for (i = 0; i < h; i++) {
 	vs->write_pixels(vs, row, w * ds_get_bytes_per_pixel(vs->ds));
 	row += ds_get_linesize(vs->ds);
