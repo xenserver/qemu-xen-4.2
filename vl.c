xenserver/qemu-xen-4.2

commit 68af5b5191593223709680b89951dfc84a32bee8

qemu_timer_pending/qemu_get_timer: cope with NULL timers
qemu_timer_pending and qemu_get_timer: don't crash if the timer passed
as an argument is NULL.

[ Fixes regression: 13b06e700528 broke save/restore on Xen. -iwj ]

Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

vl.c
@@ -1201,6 +1201,10 @@ void qemu_mod_timer(QEMUTimer *ts, int64_t expire_time)
 int qemu_timer_pending(QEMUTimer *ts)
 {
     QEMUTimer *t;
+
+    if (ts == NULL)
+        return 0;
+
     for(t = active_timers[ts->clock->type]; t != NULL; t = t->next) {
         if (t == ts)
             return 1;
@@ -1271,6 +1275,9 @@ void qemu_put_timer(QEMUFile *f, QEMUTimer *ts)
 void qemu_get_timer(QEMUFile *f, QEMUTimer *ts)
 {
     uint64_t expire_time;
+
+    if (ts == NULL)
+        return;
 
     expire_time = qemu_get_be64(f);
     if (expire_time != -1) {

commit 244959f65fc67b24a00265110495a555d92f533a

qemu_get_timer: always read the 64 bit value from the savefile
qemu_get_timer: the 64 bit value needs to be read from the save file
even when the timer is not initialized otherwise following reads from
the savefile will read the wrong fields.

[ This fixes a regression in 68af5b51 -iwj. ]

Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

vl.c
@@ -1276,10 +1276,10 @@ void qemu_get_timer(QEMUFile *f, QEMUTimer *ts)
 {
     uint64_t expire_time;
 
+    expire_time = qemu_get_be64(f);
+
     if (ts == NULL)
         return;
-
-    expire_time = qemu_get_be64(f);
     if (expire_time != -1) {
         qemu_mod_timer(ts, expire_time);
     } else {
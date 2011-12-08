244959f65fc67b24a00265110495a555d92f533a
Ian Jackson <ian.jackson@eu.citrix.com>
2011-12-08 16:36:03 +0000

    qemu_get_timer: always read the 64 bit value from the savefile
    qemu_get_timer: the 64 bit value needs to be read from the save file
    even when the timer is not initialized otherwise following reads from
    the savefile will read the wrong fields.
    
    [ This fixes a regression in 68af5b51 -iwj. ]
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- vl.c
+++ vl.c
@@ -1276,10 +1276,10 @@ void qemu_get_timer(QEMUFile *f, QEMUTimer *ts)
 {
     uint64_t expire_time;
 
+    expire_time = qemu_get_be64(f);
+
     if (ts == NULL)
         return;
-
-    expire_time = qemu_get_be64(f);
     if (expire_time != -1) {
         qemu_mod_timer(ts, expire_time);
     } else {
